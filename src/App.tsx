3d5b9ac86dadce193dfd7b9fbba628e43bdff732
unknown <ramirezyuli412@gmail.com>
2025-04-28 17:43:07 -0500

    Installing and using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ function App() {
 
   return (
     <>
+    <header className=" bg-teal-400 py-5">
+      <h1 className=" text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+    </header>
     </>
   )
 }
